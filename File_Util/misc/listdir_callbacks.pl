4638bd0ce1b3cf4b23032a78096fe02c2ec6f49b
Tommy Butler <ace@tommybutler.com>
2013-01-29 12:38:53 -0600

    changed misc/listdir_callbacks.pl to use the actual project directory for its callbacks instead of some random temp dir, which makes the printouts more developer-palatable.  Cleaned up its output as well.  Looks nice.  Going to probably put it into the examples/cookbook in some altered form

--- File_Util/misc/listdir_callbacks.pl
+++ File_Util/misc/listdir_callbacks.pl
@@ -4,94 +4,58 @@ use warnings;
 use strict;
 use 5.10.0;
 
-use Time::HiRes;
-use File::Temp qw( tempdir );
-
 use lib './lib';
-use File::Util qw( SL );
+use File::Util;
 
 my $ftl = File::Util->new( { fatals_as_status => 1 } );
 
-my $tempdir = tempdir( CLEANUP => 1 );
+sub sayindent {
+   my ( $indent, $text ) = @_;
+   say( ( ' ' x ( $indent * 3 ) ) . $text );
+}
 
-setup_test_tree();
-
-my $d_cb = sub
+sub dirs_callback
 {
-   my ( $dir, $subdirs ) = @_;
+   my ( $dir, $subdirs, $depth ) = @_;
 
-   print qq(SUBDIRS IN $dir\n);
-   print "   - none\n\n" and return unless @$subdirs;
-   print "   - $_\n" for @$subdirs;
+   sayindent( $depth,  qq(  SUBDIRS IN $dir) );
+   sayindent( $depth,  "    - none" ) and return unless @$subdirs;
+   sayindent( $depth, "    - $_" ) for @$subdirs;
+
+   return;
+};
+
+sub files_callback
+{
+   my ( $dir, $files, $depth ) = @_;
+
+   sayindent( $depth, qq(  FILES in $dir) );
+   sayindent( $depth, "    - none\n" ) and return unless @$files;
+   sayindent( $depth, "    - $_" ) for @$files;
    print "\n";
 
    return;
 };
 
-my $f_cb = sub
+sub callback
 {
-   my ( $dir, $files ) = @_;
-
-   print qq(FILES in $dir\n);
-   print "   - none\n\n" and return unless @$files;
-   print "   - $_ - @{[ -s $_ ]} bytes\n" for @$files;
-   print "\n";
-
-   return;
-};
-
-my $cb = sub {
-   my ( $dir, $subdirs, $files ) = @_;
+   my ( $dir, $subdirs, $files, $depth ) = @_;
 
    $subdirs = scalar @$subdirs;
    $files   = scalar @$files;
 
-   say qq(TOTALS IN: $dir - $subdirs directories | $files files\n);
+   sayindent( $depth, '+' . ( '-' x 70 ) );
+   sayindent( $depth, qq(| IN $dir - $subdirs sub-directories | $files files | $depth DEEP) );
+   sayindent( $depth, '+' . ( '-' x 70 ) );
 };
 
 $ftl->list_dir(
-   $tempdir => {
-      d_callback  => $d_cb,
-      f_callback  => $f_cb,
-      callback    => $cb,
-      recurse     => 1, # set to zero if you want to see diff output
-      with_paths  => 1, # unnecessary if recurse => 1
-      no_fsdots   => 1, # unnecessary if recurse => 1
+   '.' => {
+      d_callback  => \&dirs_callback,
+      f_callback  => \&files_callback,
+      callback    => \&callback,
+      recurse     => 1,
    }
 );
 
 exit;
-
-sub setup_test_tree {
-
-   my @test_files  = qw(
-      a.txt   b.log
-      c.ini   d.bat
-      e.sh    f.conf
-      g.bin   h.rc
-   );
-
-   for my $tfile ( @test_files )
-   {
-      $ftl->touch( $tempdir . SL . $tfile );
-   }
-
-   my $deeper = $tempdir . SL . 'xfoo' . SL . 'zbar';
-
-   $ftl->make_dir( $deeper );
-
-   @test_files = qw(
-      i.jpg   j.xls
-      k.ppt   l.scr
-      m.html  n.js
-      o.css   p.avi
-   );
-
-   for my $tfile ( @test_files )
-   {
-      $ftl->write_file( { file => $deeper . SL . $tfile, content => rand } );
-   }
-
-   return;
-}
-
