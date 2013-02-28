08be2af171a12c11a8407ff1e6256915396b6a7b
Tommy Butler <ace@tommybutler.com>
2013-02-27 21:20:13 -0600

    fixed brewtest.sh so it would stop spawning subshells

--- File_Util/misc/brewtest.sh
+++ File_Util/misc/brewtest.sh
@@ -2,6 +2,8 @@
 
 dzil clean >/dev/null && dzil build >/dev/null;
 
+. ~/perl5/perlbrew/etc/bashrc
+
 for brew in 5.10.1  5.12.5  5.14.3  5.16.2  5.17.7  5.17.8  5.17.9  5.8.9; do
 
    perlbrew use perl-$brew;
