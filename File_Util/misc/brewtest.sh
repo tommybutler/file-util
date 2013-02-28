5c69d545776717ba14f36a25238dccc2d0fa50f5
Tommy Butler <ace@tommybutler.com>
2013-02-27 21:05:56 -0600

    added a perlbrew test script (shell script) to do brew-testing after Dist::Zilla::Plugin::Test::LocalBrew stopped working today

---
+++ File_Util/misc/brewtest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+dzil clean >/dev/null && dzil build >/dev/null;
+
+for brew in 5.10.1  5.12.5  5.14.3  5.16.2  5.17.7  5.17.8  5.17.9  5.8.9; do
+
+   perlbrew use perl-$brew;
+
+   brewtmp="/tmp/brewtest/$( date +%s )/$brew/";
+
+   mkdir -p $brewtmp;
+
+   echo Trying in $brew ...;
+
+   cpanm -L $brewtmp File-Util-*gz >/dev/null 2>&1 && echo "OK in $brew" || "FAILED IN $brew";
+
+   rm -rf $brewtmp;
+
+done
